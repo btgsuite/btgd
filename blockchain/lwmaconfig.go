0de208e60f734bc4e72febc0f6fc1930b2788bf7
Vutov <VutovSpas@gmail.com>
2019-02-23 20:09:47 +0200

    added lwma tests

--- blockchain/lwmaconfig.go
+++ blockchain/lwmaconfig.go
@@ -17,3 +17,53 @@ type LwmaConfig struct {
 	solveTimeLimitation bool     // Solve time limitation
 	powLimit            *big.Int // Pow limit
 }
+
+func createPow(value string) *big.Int {
+	powLimit, _ := new(big.Int).SetString(value, 0)
+	return powLimit
+}
+
+// GetMainnetLWMA returns Mainnet config for LWMA
+func GetMainnetLWMA() LwmaConfig {
+	return LwmaConfig{
+		enableHeight:        536200,
+		testnet:             false,
+		regtest:             false,
+		powTargetSpacing:    600,
+		averagingWindow:     45,
+		adjustWeight:        13772,
+		minDenominator:      10,
+		solveTimeLimitation: true,
+		powLimit:            createPow("14134776517815698497336078495404605830980533548759267698564454644503805952"),
+	}
+}
+
+// GetTestnetLWMA returns Testnet config for LWMA
+func GetTestnetLWMA() LwmaConfig {
+	return LwmaConfig{
+		enableHeight:        14300,
+		testnet:             true,
+		regtest:             false,
+		powTargetSpacing:    600,
+		averagingWindow:     45,
+		adjustWeight:        13772,
+		minDenominator:      10,
+		solveTimeLimitation: false,
+		powLimit:            createPow("14134776518227074636666380005943348126619871175004951664972849610340958207"),
+	}
+}
+
+// GetRegtestLWMA returns Regtest config for LWMA
+func GetRegtestLWMA() LwmaConfig {
+	return LwmaConfig{
+		enableHeight:        0,
+		testnet:             false,
+		regtest:             true,
+		powTargetSpacing:    600,
+		averagingWindow:     45,
+		adjustWeight:        13772,
+		minDenominator:      10,
+		solveTimeLimitation: false,
+		powLimit:            createPow("57896044618658097711785492504343953926634992332820282019728792003956564819967"),
+	}
+}
